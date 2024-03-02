f26f9c0be6b91f8da681ea83705f1a671e00537f
salita <unidadsanitarialastoninas237@gmail.com>
2024-03-02 14:43:16 -0300

    add vercel.json

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import express from "express"
 import cors from "cors"
 import morgan from "morgan"
 import turnRoutes from "./routes/turns.js"
+import "dotenv/config"
 
 const app = express()
+const PORT = process.env.PORT || 3000
 app.use(cors())
 app.use(morgan("dev"))
 app.use(express.json())
@@ -11,6 +13,6 @@ app.use(express.json())
 app.use("/",turnRoutes)
 
 
-app.listen(3000, ()=>{
-    console.log("server on http://localhost:3000")
+app.listen(PORT, ()=>{
+    console.log(`server on http://localhost:${PORT}`)
 })
