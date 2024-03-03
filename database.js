e1b9019afcba15846571dd98e30e68026801a1ed
Unidad-Sanitaria-Las-Toninas <161946878+Unidad-Sanitaria-Las-Toninas@users.noreply.github.com>
2024-03-03 20:31:07 -0300

    Add files via upload

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL ,
+  connectionString: process.env.POSTGRES_URL || "postgres://postgres:santiago45@localhost:5432/Salita",
 })
 export default pool;
 
